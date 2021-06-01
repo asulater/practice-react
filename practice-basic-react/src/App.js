9d58dd534a62e39b36132a2e0128fd35985d03b7
asulater <asulater@gmail.com>
2021-06-01 23:06:47 +0900

    updates

--- practice-basic-react/src/App.js
+++ practice-basic-react/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Practice basic react
         </a>
       </header>
     </div>
